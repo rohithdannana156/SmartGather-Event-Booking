d85c4d806dfb58354458e6685175f08c4c5bc29a
rohithdannana156 <rohithdannana@918203@gmail.com>
2026-08-22 18:05:54 +0530

    Fix Nodemailer ETIMEDOUT by using explicit SMTP host

--- server/utils/email.js
+++ server/utils/email.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     secure: false, // true for 465, false for 587
     auth: {
         user: process.env.EMAIL_USER,
